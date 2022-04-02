a93969fe98a40287d84053e2771df8d18f1a6c98
Vera Ma <86516036+VeraIsMay@users.noreply.github.com>
2022-04-02 20:35:50 +0800

    Add files via upload

---
+++ newgame/logweb/forms.py
@@ -0,0 +1,9 @@
+# in this file I only create form for register for now
+# the form for login was written manually in the html
+from django import forms
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(label="nickname", max_length=10, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="password", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="confirm password", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="email address", widget=forms.EmailInput(attrs={'class': 'form-control'}))
