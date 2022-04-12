8ccdaa2594f00ff98a6e85abce62e770b6b34763
Vera Ma <86516036+VeraIsMay@users.noreply.github.com>
2022-04-12 15:33:16 +0800

    Update admin.py

--- newgame/game/admin.py
+++ newgame/game/admin.py
@@ -2,3 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 from . import models
+admin.site.register(models.Character)
+admin.site.register(models.Student)
+admin.site.register(models.Item)
+admin.site.register(models.mObject)
+admin.site.register(models.Bubble)
+admin.site.register(models.Weapon)
+admin.site.register(models.Skill)
+admin.site.register(models.OnMap)
+admin.site.register(models.Npc)
+admin.site.register(models.Projectile)
+admin.site.register(models.Melee)
+admin.site.register(models.Area_effect)
